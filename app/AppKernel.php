06e1da19712031076dcf8bebca38377b917bb8d0
quotidianus <github@b67.me>
2017-04-21 16:42:35 +0200

    Add StofDoctrineExtensionsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             // Librinfo
             new Librinfo\EmailBundle\LibrinfoEmailBundle(),
             new Librinfo\MediaBundle\LibrinfoMediaBundle(),
